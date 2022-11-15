25fbe6710d643337c5d0baed63b7f00f4e47cf51
Angie-Santos <ajsag12@gmail.com>
2022-11-15 11:25:30 -0500

    Estilos mobile estandar componente login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,25 +4,23 @@ import './styles/Login.scss';
 function Login() {
   return (
     <div className='login'>
-      <div className='login__container-form'>
-        <img
-          className='login__container-form-logo'
-          src='src/assets/logo.png'
-          alt='Logo'
-        />
-        <form action=''>
-          <h1>Bienvenido</h1>
-          <label htmlFor='login-correo'>
-            Correo
-            <input type='text' id='login-correo' required />
-          </label>
-          <label htmlFor='login-contraseña'>
-            Contraseña
-            <input type='password' id='login-contraseña' required />
-          </label>
-          <button type='submit'>Ingresa</button>
-        </form>
-      </div>
+      <img
+        className='login__container-form-logo'
+        src='src/assets/logo.png'
+        alt='Logo'
+      />
+      <form action=''>
+        <h1>Bienvenido</h1>
+        <label htmlFor='login-correo'>
+          Correo
+          <input type='text' id='login-correo' required />
+        </label>
+        <label htmlFor='login-contraseña'>
+          Contraseña
+          <input type='password' id='login-contraseña' required />
+        </label>
+        <button type='submit'>Ingresa</button>
+      </form>
     </div>
   );
 }
