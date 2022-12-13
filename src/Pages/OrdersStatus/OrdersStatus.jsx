45b0a6de3d9f774796e7bbe7bc8e4f3be2edc498
Amappola <anam.porrasp@gmail.com>
2022-12-13 16:29:20 -0500

    Guardando el valor del select de OrdersStatus

--- src/Pages/OrdersStatus/OrdersStatus.jsx
+++ src/Pages/OrdersStatus/OrdersStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import UserContext from '../../../context/User/UserProvider';
 import NavBar from '../../components/NavBar/NavBar';
@@ -6,6 +6,7 @@ import './OrdersStatus.scss';
 
 function OrdersStatus() {
   const { user, navBarContext } = useContext(UserContext);
+  const [valueSelect, setValueSelect] = useState('pending');
 
   const navBarOptions = (role) => {
     let component;
@@ -87,8 +88,53 @@ function OrdersStatus() {
 
     return component;
   };
+
+  const handleSelect = (ev) => {
+    setValueSelect(ev.target.value);
+  };
+
   return (
-    <section className='orders-status'>{navBarOptions(user.role)}</section>
+    <section className='orders-status'>
+      {navBarOptions(user.role)}
+      <div
+        className={
+          navBarContext === true
+            ? 'orders-status__container--closed'
+            : 'orders-status__container'
+        }
+      >
+        <select name='' id='' onChange={(ev) => handleSelect(ev)}>
+          <option value='pending'>--Estado de la Órden--</option>
+          <option value='pending'>Pendientes</option>
+          <option value='finished'>Terminadas</option>
+          <option value='delivered'>Entregadas</option>
+        </select>
+        <table className='orders-status__pending-orders'>
+          <thead>
+            <tr>
+              <th>Pendientes</th>
+            </tr>
+          </thead>
+          <tbody>Cuerpo de la tabla</tbody>
+        </table>
+        <table className='orders-status__finished-orders'>
+          <thead>
+            <tr>
+              <th>Terminadas</th>
+            </tr>
+          </thead>
+          <tbody>Cuerpo de la tabla</tbody>
+        </table>
+        <table className='orders-status__delivered-orders'>
+          <thead>
+            <tr>
+              <th>Entregadas</th>
+            </tr>
+          </thead>
+          <tbody>Cuerpo de la tabla</tbody>
+        </table>
+      </div>
+    </section>
   );
 }
 
