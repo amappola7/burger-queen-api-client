c6d1a692cceb3056d87a21aa9ca6bb0b63ee715c
Angie-Santos <ajsag12@gmail.com>
2022-12-15 16:58:30 -0500

    Arreglando error del create root

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
